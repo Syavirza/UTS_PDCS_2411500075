c2de9545f4397c2fe55323fb47945dfd9fa2c7c2
Syavirza <2411500075@mahasiswa.atmaluhur.ac.id>
2025-11-12 22:37:09 +0700

    1.

--- add_book.php
+++ add_book.php
@@ -1,3 +1,30 @@
+<?php
+require_once 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  $judul = $_POST['judul'];
+  $penulis = $_POST['penulis'];
+  $penerbit = $_POST['penerbit'];
+  $tahun_terbit = $_POST['tahun_terbit'];
+  $stock = $_POST['stock'];
+
+  $sql = "INSERT INTO buku (judul, penulis, penerbit, tahun_terbit, stock) VALUES (?, ?, ?, ?, ?)";
+
+  if ($stmt = $mysqli->prepare($sql)){
+    $stmt->bind_param("sssii", $judul, $penulis, $penerbit, $tahun_terbit, $stock);
+    if ($stmt->execute()){
+      header("location: buku.php");
+      exit();
+    } else {
+      echo "Error: Gagal menyimpan data.";
+    }
+    $stmt->close();
+  }
+  $mysqli->close();
+}
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,7 +34,33 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+   <div class="container mt-4">
+    <h2>Tambah buku Baru</h2>
+    <form action="add_book.php" method="post">
+        <div class="mb-3">
+                <label for="judul" class="form-label">judul</label>
+                <input type="text" class="form-control" id="judul" name="judul" required>
+        </div>
+        <div class="mb-3">
+                <label for="penulis" class="form-label">penulis</label>
+                <input type="text" class="form-control" id="penulis" name="penulis" required>
+        </div>
+        <div class="mb-3">
+                <label for="tahun_terbit" class="form-label">tahun terbit</label>
+                <input type="number" class="form-control" id="tahun_terbit" name="tahun_terbit" required>
+        </div>
+        <div class="mb-3">
+                <label for="penerbit" class="form-label">penerbit</label>
+                <input type="text" class="form-control" id="penerbit" name="penerbit" required>
+        </div>
+        <div class="mb-3">
+                <label for="stock" class="form-label">stock</label>
+                <input type="number" class="form-control" id="stock" name="stock" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+        <a href="buku.php"class="btn btn-secondary">Batal</a>
+    </form>
+   </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
 </html>
